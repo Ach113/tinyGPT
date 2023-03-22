b9aa8937f4e7fd3efefaff2f65eae9aed105f595
ach113 <archieberidze@gmail.com>
2023-03-21 23:29:34 -0700

    add scaled hyperparameters

--- src/config.py
+++ src/config.py
@@ -2,6 +2,11 @@ import torch
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-NUM_EMBED = 32
-BLOCK_SIZE = 8
-NUM_HEADS = 4
+NUM_EMBED = 384  # must be divisible by `NUM_HEADS`
+BLOCK_SIZE = 256
+NUM_HEADS = 6
+DROPOUT = 0.2
+NUM_BLOCKS = 6
+LEARNING_RATE = 3e-4
+NUM_EPOCHS = 5000
+BATCH_SIZE = 64
